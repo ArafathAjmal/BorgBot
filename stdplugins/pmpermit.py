6297e4ebd422d97547e3e39743a57977be0e97ad
ArafathAjmal <51064898+ArafathAjmal@users.noreply.github.com>
2019-10-06 07:35:13 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -19,9 +19,9 @@ BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
 TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
 UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
 UNIBORG_USER_BOT_NO_WARN = ("`Bleep blop! This is a bot. Don't fret.`\n\n"
-                  "`If you have any query then you should go to `"
-                  "@TheVincentArmy.\n\n"
-                  "`You will find the solutions to all your problems there.`")
+                  "`If you have any query then you msg at  `"
+                  "@YourGrootBot .\n\n"
+                  "`I will Reply ASAP. `")
 
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
